78d48bd8395ee25ade579d4abdb0735fca139423
Gilles HOPIN <gilleshopin@yahoo.com>
2023-06-21 22:38:47 +0200

    Revert "Revert "compilation script added" (test pour passer la compilation"
    This reverts commit e9af80d172594ebdb2037e660574e768d2bb2649.

---
+++ compilation.sh
@@ -0,0 +1,7 @@
+./build.py crash-consensus
+crash-consensus/libgen/export.sh gcc-release
+crash-consensus/demo/using_conan_fully/build.sh gcc-release
+crash-consensus/experiments/build.sh
+crash-consensus/experiments/liquibook/build.sh
+export LD_LIBRARY_PATH=$HOME/dory/crash-consensus/experiments/exported:$LD_LIBRARY_PATH
+echo "export LD_LIBRARY_PATH=$HOME/dory/crash-consensus/experiments/exported:$LD_LIBRARY_PATH" >> ~/.bashrc
