90c3779e697a86bbd9f2209867f0d47ceed3f8bf
Gilles HOPIN <gilleshopin@yahoo.com>
2023-06-26 10:05:03 +0200

    compile_debug added

---
+++ compile_debug.sh
@@ -0,0 +1,8 @@
+./build.py -b debug crash-consensus
+crash-consensus/libgen/export.sh gcc-debug
+crash-consensus/demo/using_conan_fully/build.sh gcc-debug
+crash-consensus/experiments/build.sh
+crash-consensus/experiments/liquibook/build.sh
+export LD_LIBRARY_PATH=$HOME/dory/crash-consensus/experiments/exported:$LD_LIBRARY_PATH
+echo "export LD_LIBRARY_PATH=$HOME/dory/crash-consensus/experiments/exported:$LD_LIBRARY_PATH" >> ~/.bashrc
+
