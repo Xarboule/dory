aebdad4d8dda74ffde6a5e1f82a0334b957ffeb4
Gilles HOPIN <gilleshopin@yahoo.com>
2023-07-03 14:35:50 +0200

    rdma cm client part again

--- conn/src/rc.cpp
+++ conn/src/rc.cpp
@@ -135,15 +135,16 @@ void ReliableConnection::associateWithCQ_for_cm_prel(std::string send_cp_name,
 
 /*Une fois le connection manager prêt, on peut enfin créer une qp
 C'est comme ça qu'on évite de devoir nous même utiliser les ibv_modify_qp()*/
-void ReliableConnection::associateWithCQ_for_cm(rdma_cm_id* id) {
+void ReliableConnection::associateWithCQ_for_cm(rdma_cm_id* &id) {
   LOGGER_INFO(logger, "Inside associateWithCQ_for_cm");
 
   int ret = rdma_create_qp(id, get_pd(), get_init_attr() );
+  /*
   if (ret) {
     printf("Failed to create QP due to errno: %s\n", strerror(errno));
     throw std::runtime_error("Failed to create QP due to %");
     return;
-  }
+  }*/
 
   /*Copié-collé de associateWithCQ() pour renseigner uniq_qp
   Il est notamment utilisé dans les send et receive */
