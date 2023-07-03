3360b7151f0904f45d830681322ea7f09af0533a
Gilles HOPIN <gilleshopin@yahoo.com>
2023-07-03 13:56:55 +0200

    rdma cm client part again

--- conn/src/exchanger.cpp
+++ conn/src/exchanger.cpp
@@ -331,7 +331,7 @@ int ConnectionExchanger:: start_client(int proc_id){
 
 	 /* Resolves an RDMA route to the destination address in order to
 	  * establish a connection */
-	ret = rdma_resolve_route(event_copy->id, 2000);
+	ret = rdma_resolve_route(event_copy.id, 2000);
 	if (ret) {
 		throw std::runtime_error("Failed to resolve route");
 	   exit(-1);
@@ -359,13 +359,13 @@ int ConnectionExchanger:: start_client(int proc_id){
   auto& rc = rcs.find(proc_id)->second;
 
   /*Creating the QP*/
-  rc.associateWithCQ_for_cm(event_copy->id);
+  rc.associateWithCQ_for_cm(event_copy.id);
 
 
   /*Connecting*/
   struct rdma_conn_param cm_params;
   memset(&cm_params, 0, sizeof(cm_params));
-  rdma_connect(event_copy->id, &cm_params);
+  rdma_connect(event_copy.id, &cm_params);
 
   LOGGER_INFO(logger, "waiting for cm event: RDMA_CM_EVENT_ESTABLISHED\n");
   ret = process_rdma_cm_event(cm_event_channel,RDMA_CM_EVENT_ESTABLISHED,&cm_event);
