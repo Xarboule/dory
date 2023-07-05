c4ffad3530f0c010ee40c151780ae10e490bbb30
Gilles HOPIN <gilleshopin@yahoo.com>
2023-07-05 13:33:45 +0200

    testing consensus

--- crash-consensus/demo/src/main-st.cpp
+++ crash-consensus/demo/src/main-st.cpp
@@ -136,8 +136,9 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
 
           case dory::ProposeError::MutexUnavailable:
           case dory::ProposeError::FollowerMode:
-            //std::cout << "Error: in follower mode. Potential leader: "
-            //          << consensus.potentialLeader() << std::endl;
+            std::cout << "Error: in follower mode. Potential leader: "
+                       << consensus.potentialLeader() << std::endl;
+            std::cout << i << endl;
             break;
 
           default:
