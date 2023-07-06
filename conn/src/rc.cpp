a42447905b381d84293f784779363d193a38ade4
Gilles HOPIN <gilleshopin@yahoo.com>
2023-07-06 14:32:49 +0200

    rc print all

--- conn/src/rc.cpp
+++ conn/src/rc.cpp
@@ -563,9 +563,9 @@ void ReliableConnection :: print_all_infos(){
   printf("\t lkey : %d \n",  mr.lkey );
   
   printf("Queue pair uniq_qp : %p \n", reinterpret_cast<void*>(&uniq_qp));
-  printf("\t qp-> context : %p \n", reinterpret_cast<void*>(uniq_qp.context));
-  printf("\t qp-> pd : %p \n", reinterpret_cast<void*>(uniq_qp.pd));
-  printf("\t qp-> context : %d \n",uniq_qp.qpn);
+  printf("\t qp-> context : %p \n", reinterpret_cast<void*>(uniq_qp->context));
+  printf("\t qp-> pd : %p \n", reinterpret_cast<void*>(uniq_qp->pd));
+  printf("\t qp-> context : %d \n",uniq_qp->qpn);
     //??
 
   printf("Remote connection rconn: %p \n", reinterpret_cast<void*>(&rconn));
