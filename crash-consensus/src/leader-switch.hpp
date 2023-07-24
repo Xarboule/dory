7b2449d8b2f757e4bb0155fcf0517c54b638f10d
Gilles HOPIN <gilleshopin@yahoo.com>
2023-07-24 11:37:10 +0200

    testing heartbeat

--- crash-consensus/src/leader-switch.hpp
+++ crash-consensus/src/leader-switch.hpp
@@ -96,6 +96,23 @@ class LeaderHeartbeat {
       outstanding_pids.insert(my_id);
     }
 
+    //test : est-ce que j'arrive à faire un RDMA READ de ma propre valeur ? (READ par ma loopback)
+    std::cout << "Posting a local Read to my own heartbeat" << std::endl; 
+    auto post_ret = loopback->postSendSingle(
+      ReliableConnection::RdmaRead, 
+      quorum::pack(quorum::LeaderHeartbeat, my_id, read_seq), 
+      slots[my_id], //where to store the content read
+      sizeof(uint64_t),
+      loopback->remoteBuf() + offset); //where to read
+    
+
+    if (!post_ret) {
+      std::cout << "(Error in posting read request to my own heartbeat) Post returned " << post_ret << std::endl;
+    }
+    
+    
+
+
     bool did_work = false;
     auto &rcs_ = *rcs;
     for (auto &[pid, rc] : rcs_) { 
@@ -123,7 +140,7 @@ class LeaderHeartbeat {
       std::cout << "polling PID = " << pid <<"; my id = "<< my_id << std::endl;
       std::cout << "About the rc : " << std::endl; 
       rc.print_all_infos();
-      std::cout << "Where to read (without offset): " << rc.remoteBuf()+offset<< std :: endl;
+      std::cout << "Where to read : " << rc.remoteBuf()+offset<< std :: endl;
     }
 
     if (did_work) { //?? 
@@ -148,9 +165,9 @@ class LeaderHeartbeat {
         auto proc_post_id = post_ids[pid];
 
         volatile uint64_t *val = reinterpret_cast<uint64_t *>(slots[pid]); //on récupère la valeur
-        if (pid == my_id) {
+        /*if (pid == my_id) {
           val = reinterpret_cast<uint64_t *>(loopback->remoteBuf() + offset); //si c'est la mienne, c'est un peu spécial 
-        }
+        }*/
         std::cout << "Polling PID: " << pid << ", PostID: " << proc_post_id << ", Value: " << *val << std::endl;
 
         if (status[pid].value == *val) { //si la valeur du heartbeat est la même qu'avant
