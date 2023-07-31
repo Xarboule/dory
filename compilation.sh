7d3d35114aa1ca4036dd6ec6ce1d133f23fbdb15
GillesHOP <79443350+GillesHOP@users.noreply.github.com>
2023-07-31 13:13:30 +0200

    Update compilation.sh

--- compilation.sh
+++ compilation.sh
@@ -5,7 +5,3 @@ crash-consensus/experiments/build.sh
 crash-consensus/experiments/liquibook/build.sh
 export LD_LIBRARY_PATH=$HOME/dory/crash-consensus/experiments/exported:$LD_LIBRARY_PATH
 echo "export LD_LIBRARY_PATH=$HOME/dory/crash-consensus/experiments/exported:$LD_LIBRARY_PATH" >> ~/.bashrc
-
-
-cd demo/
-./build.sh
