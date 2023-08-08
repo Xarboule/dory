f6624f67bc6a440c600f3153969faa924598d274
Gilles HOPIN <gilleshopin@yahoo.com>
2023-08-08 15:51:46 +0200

    integrating byp4ss

--- conn/src/rc.hpp
+++ conn/src/rc.hpp
@@ -11,7 +11,7 @@
 #include <dory/extern/ibverbs.hpp>
 #include <dory/extern/rdmacm.hpp>
 
-#include "../../libbypass-main/bypass.h"
+#include "bypass.h"
 
 namespace dory {
 struct RemoteConnection {
