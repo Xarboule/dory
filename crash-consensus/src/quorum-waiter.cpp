45ed88195d7d4e27f4becea26571fa708f935b9d
Gilles HOPIN <gilleshopin@yahoo.com>
2023-08-10 17:58:40 +0200

    more details entry statys

--- crash-consensus/src/quorum-waiter.cpp
+++ crash-consensus/src/quorum-waiter.cpp
@@ -100,7 +100,7 @@ template <class ID> bool SerialQuorumWaiter<ID>::fastConsume(std::vector<struct 
 
     //std::cout << "The status is " << ibv_wc_status_str(entry.status)  << std::endl;
     if (entry.status != IBV_WC_SUCCESS) {
-      std::cout << "In fastConsume, not IBV_WC_SUCCESS for the entry, instead :  " << entry.status << std::endl;
+      std::cout << "In fastConsume, not IBV_WC_SUCCESS for the entry, instead :  " << ibv_wc_status_str(entry.status) << std::endl;
       return false;
     } else {
       auto [k, pid, seq] = quorum::unpackAll<int, ID>(entry.wr_id);
