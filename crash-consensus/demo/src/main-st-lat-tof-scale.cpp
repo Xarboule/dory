8e523b77fc3a8c11e0128eafb41ea17844cfa8f9
Gilles HOPIN <gilleshopin@yahoo.com>
2023-08-10 12:18:51 +0200

    printing proposal number of fail

--- crash-consensus/demo/src/main-st-lat-tof-scale.cpp
+++ crash-consensus/demo/src/main-st-lat-tof-scale.cpp
@@ -134,14 +134,14 @@ void benchmark(int id, std::vector<int> remote_ids, int times, int payload_size,
       //std::cout << "Next propose ? ";
       //std::cin >> foo;
       
-      std::cout << "Proposing " << i << std::endl;
+      //std::cout << "Proposing " << i << std::endl;
       GET_TIMESTAMP(timestamps_start[i]);
 
       // Encode process doing the proposal
       dory::ProposeError err;
       // std::cout << "Proposing " << i << std::endl;
       if ((err = consensus.propose(&(payloads[i % 8192][0]), payload_size)) != dory::ProposeError::NoError) {
-        // std::cout << "Proposal failed at index " << i << std::endl;
+        std::cout << "Proposal failed at index " << i << std::endl;
         i -= 1;
         switch (err) {
           case dory::ProposeError::FastPath:
